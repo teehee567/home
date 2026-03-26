6512ec7abf0451ff196a1754f0bbb6706ea78dc2
teehee567 <teehee567@users.noreply.github.com>
2026-03-27 10:21:16 +1100

    send framedstream

--- crates/transport/src/framed.rs
+++ crates/transport/src/framed.rs
@@ -1,8 +1,7 @@
 use anyhow::Result;
-use std::future::Future;
 
+#[trait_variant::make(Send)]
 pub trait FramedStream {
-    fn send(&mut self, data: &[u8]) -> impl Future<Output = Result<()>> + Send;
-    fn receive(&mut self) -> impl Future<Output = Result<Vec<u8>>> + Send;
+    async fn send(&mut self, data: &[u8]) -> Result<()>;
+    async fn receive(&mut self) -> Result<Vec<u8>>;
 }
-
